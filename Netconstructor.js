bb16349c8c11c3776f0b0f01eb6f5be7f45fd4b9
Unknown <bo4arovpavel1989@mail.ru>
2018-01-26 09:21:38 +0300

    added calculation for output

--- Netconstructor.js
+++ Netconstructor.js
@@ -83,25 +83,36 @@ NeuroNet.prototype.train = function(data){
 	
 	for (var {input, output} of data) {
 		
-	//start calculation first hidden layer. coz it always exists
-	for (let j = 0; j < this.options.hidden_size; j++){
-		var h_input=0;
-		for (let k =0; k < input.length; k++){
-			h_input += this.weights[`i${k}_h0_${j}`]
-		}	
-		h_input += this.weights[`bias_h0_${j}`];
-		nodes[`h0_${j}`] = activation[this.options.activation](h_input);  
-	}  
+		//start calculation first hidden layer. coz it always exists
+		for (let j = 0; j < this.options.hidden_size; j++){
+			let h_input=0;
+			for (let k =0; k < input.length; k++){
+				h_input += this.weights[`i${k}_h0_${j}`]*input[k];
+			}	
+			h_input += this.weights[`bias_h0_${j}`];
+			nodes[`h0_${j}`] = activation[this.options.activation](h_input);  
+		}  
 
-	//start calculation other hidden layer.
-	for (j = 0; j < this.options.hidden - 1; j++)
-		for (k = 0; k < this.options.hidden_size; k++){
-			var h_input = 0;
-			for (m = 0; m < this.options.hidden_size; m++){
-				h_input += this.weights[`h${j}_${m}_h${j+1}_${k}`];
+		//start calculation other hidden layer.
+		for (let j = 0; j < this.options.hidden - 1; j++){
+			for (let k = 0; k < this.options.hidden_size; k++){
+				let h_input = 0;
+				for (let m = 0; m < this.options.hidden_size; m++){
+					h_input += this.weights[`h${j}_${m}_h${j+1}_${k}`]*nodes[`h${j}_${m}`];
+				}
+				h_input += this.weights[`bias_h${j+1}_${k}`];
+				nodes[`h${j+1}_${k}`] = activation[this.options.activation](h_input);
 			}
-			h_input += this.weights[`bias_h${j+1}_${k}`];
-			nodes[`h${j+1}_${k}`] = activation[this.options.activation](h_input);
+		}	
+		
+		//start calculation output layer.
+		for (let j = 0; j < this.options.output; j++) {
+			let o_input = 0;
+			for (let k = 0; k < this.options.hidden_size; k++){
+				o_input += this.weights[`h${this.options.hidden - 1}_${k}_o${j}`]*nodes[`h${this.options.hidden - 1}_${k}`];
+			}
+			o_input += this.weights[`bias_o${j}`];
+			nodes[`o${j}`] = activation[this.options.activation](o_input);
 		}
     }
 	console.log(nodes);
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var NeuroNet = require('./Netconstructor.js');
 var neuro = new NeuroNet();
-neuro.init({hidden:4, hidden_size:3});
+neuro.init();
 neuro.train([
     {input: [0, 0], output: 0},
     {input: [1, 0], output: 1},
