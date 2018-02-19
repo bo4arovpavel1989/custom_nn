fc8308d3fc80fa373f39538a56b763dc1f54a2b2
Unknown <bo4arovpavel1989@mail.ru>
2018-02-19 09:49:44 +0300

    added check for mnist example

--- example/mnist/example_mnist.js
+++ example/mnist/example_mnist.js
@@ -1,17 +1,25 @@
 var NeuroNet = require('./../../Netconstructor.js');
 var mnist = require('./mnist.js')
-const set = mnist.set(1000, 20);
+const set = mnist.set(8000, 20);
 
 const trainingSet = set.training;
 var testSet = set.test;
 
 var neuro = new NeuroNet();
 
+var get_i_with_max_val = function(arr){
+	let i = 0;
+	for (let j = 1; j < arr.length; j++){
+		if(arr[j] > arr[j-1]) i = j;
+	}
+	return i;
+}
 
 
 var outputResults = () => 
     testSet.forEach((item) => 
-                 console.log(`${item.input[0]} XOR ${item.input[1]} => ${neuro.run(item.input)} (expected ${item.output})`));
+                 console.log(`${item.input[0]} XOR ${item.input[1]} => ${get_i_with_max_val(neuro.run(item.input))} (expected ${get_i_with_max_val(item.output)})`));
+
 
 neuro.init({input:784,hidden_sizes:[392],output:10,console_logging:{step:1}})
 	 .train(trainingSet)
@@ -20,6 +28,10 @@ neuro.init({input:784,hidden_sizes:[392],output:10,console_logging:{step:1}})
 			outputResults(); 
 		 }); 
 	
-
-
+/*
+neuro.init()
+	 .load('mnist_net.dat')
+	 .then(()=>{
+		 outputResults(); 
+	 })*/
 
